53d3b8db6cbd3577e968b739cc485e0974946019
Reem H <reemhamoui92@gmail.com>
2019-11-28 10:53:12 -0500

    Updates

---
+++ precache-manifest.d27ca41dbd69261e95c15517b31f2f47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b984266095008d72ed668799165f6bcc",
+    "url": "/react-and-gh-pages/index.html"
+  },
+  {
+    "revision": "6f6669036aff02e79061",
+    "url": "/react-and-gh-pages/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d313c11e153b0f3f358c",
+    "url": "/react-and-gh-pages/static/js/2.8d16f4ab.chunk.js"
+  },
+  {
+    "revision": "6f6669036aff02e79061",
+    "url": "/react-and-gh-pages/static/js/main.839dbd0b.chunk.js"
+  },
+  {
+    "revision": "7ef562a0949fcdf9190b",
+    "url": "/react-and-gh-pages/static/js/runtime-main.f4d7da68.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-and-gh-pages/static/media/logo.25bf045c.svg"
+  }
+]);
